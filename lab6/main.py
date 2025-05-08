fb8d8f200954dd28d57eb9c65a403b4fb9393537
AtharvaBansod <atharvabansod12321@gmail.com>
2025-05-09 05:16:17 +0530

    lab 6

--- lab6/main.py
+++ lab6/main.py
@@ -1,3 +1,8 @@
+# Build a content-based recommender system and evaluate
+# its architecture.
+
+
+
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
